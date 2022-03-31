1c14e38fe301205cb42d34486d22f8f63e110cec
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-03-31 17:11:50 +0200

    Add files via upload

---
+++ BasicSyntax_ConditionalStatements_Loops/StudentInformation.java
@@ -0,0 +1,16 @@
+package BasicSyntax_ConditionalStatements_Loops;
+
+import java.util.Scanner;
+
+public class StudentInformation {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.02f", name, age, grade);
+    }
+}
